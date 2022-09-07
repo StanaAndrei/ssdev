9cfe021ff8c926f608dca92ee54bf73f5f6942d5
Stana Andrei <andrew@192-168-0-80.rdsnet.ro>
2022-09-07 12:16:34 +0300

    removed useless libs

--- app/src/main/java/com/example/ssdev/MainActivity.java
+++ app/src/main/java/com/example/ssdev/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.ssdev;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Environment;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
